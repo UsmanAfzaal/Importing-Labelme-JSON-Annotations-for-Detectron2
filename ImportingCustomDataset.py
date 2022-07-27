fb58fff483464f7c33eb3a98ab30891ded357eaf
UsmanAfzaal <47932947+UsmanAfzaal@users.noreply.github.com>
2022-07-27 12:36:09 +0500

    Update ImportingCustomDataset.py

--- ImportingCustomDataset.py
+++ ImportingCustomDataset.py
@@ -3,5 +3,5 @@
 from detectron2.data.datasets import register_coco_instances
 
 register_coco_instances("Dataset_train", {}, "/home/path/Dataset_train.json", "/home/path/datasetdirectory/train/") # Registers a dataset named "Dataset_train" by taking annotations from the "Dataset_train.json" file.
-register_coco_instances("Dataset_test", {}, "/home/path/Dataset_test.json", "/home/path/datasetdirectory/test/") # Registers a dataset named "Dataset_test" by taking annotations from the "Dataset_test.json" file.
+register_coco_instances("Dataset_test", {}, "/home/path/Dataset_test.json", "/home/path/datasetdirectory/test/")
 dataset_metadata = MetadataCatalog.get("Dataset_train").set(thing_classes=['Class_one', 'Class_two', 'Class_three'])
